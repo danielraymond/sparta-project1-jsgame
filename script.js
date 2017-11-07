2d081fe2a31210ccf73d334bddff48252c7aaee9
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-07 13:02:55 +0000

    edited functions that determine player position

--- script.js
+++ script.js
@@ -1,58 +1,68 @@
 $(function(){
 
-  // var location = 0;
-  //
-  // var data =  {
-  //   "text":["You are at the entrance to the ... 100 metres into the corridor/tunnel you come across two doors. Do you choose the left door or the right door?", "option2", "option3"],
-  //   "button1":["go left", "2a1", "2a2"],
-  //   "button2":["go right", "2b1", "2b2"]
-  // }
-  //
-  // function setGameListeners() {
-  //   $("#start-button").click(startGame);
-  //   $(".choice-button-1").click(choice1);
-  //   $(".choice-button-2").click(choice2);
-  //   $(".combat-roll-button").click(combat);
-  // }
-  //
-  // function startGame() {
-  //   $("p").css("visibility", "visible");
-  //   $(".choice-button-1").css("visibility", "visible");
-  //   $(".choice-button-2").css("visibility", "visible");
-  //   $(".choice-button-1").html(data.button1[0]);
-  //   $(".choice-button-2").html(data.button2[0]);
-  //   $("p").html(data.text[0]);
-  //   $(this).css("visibility", "hidden");
-  //   location ++;
-  // }
-  //
-  // function choice1() {
-  //   if (location === 1) {
-  //     $(".choice-button-1").html(data.button1[1]);
-  //     $(".choice-button-2").html(data.button2[1]);
-  //     $("p").html(data.text[1]);
-  //   }
-  //   location ++;
-  // }
-  //
-  // function choice2() {
-  //   if (location === 1) {
-  //     $(".choice-button-1").html(data.button1[2]);
-  //     $(".choice-button-2").html(data.button2[2]);
-  //     $("p").html(data.text[2]);
-  //   }
-  //   location ++;
-  // }
-
+  var locationCounter = 0;
+  var combatCounter = 0;
   var playerHealth = 12;
-  var enemyHealth = 6;
+
+  var data =  {
+    "text":["You are at the entrance to the ... 100 metres into the corridor/tunnel you come across two doors. Do you choose the left door or the right door?", "option2", "option3"],
+    "button1":["go left", "2a1", "2a2"],
+    "button2":["go right", "2b1", "2b2"],
+    "enemy":["enemy1", "boss"],
+    "enemyHealth":["6", "10"]
+  }
+
+  var enemyHealth = parseInt(data.enemyHealth[combatCounter]);
+
+  // setting listeners on game buttons and functions that it applies
+  function setGameListeners() {
+    $("#start-button").click(startGame);
+    $(".choice-button-1").click(choice);
+    $(".choice-button-2").click(choice);
+    $(".combat-roll-button").click(combat);
+  }
+
+  // function that happens when the start game button is clicked
+  function startGame() {
+    $("#decision-text").css("visibility", "visible");
+    $(".choice-button-1").css("visibility", "visible");
+    $(".choice-button-2").css("visibility", "visible");
+    $(".choice-button-1").html(data.button1[0]);
+    $(".choice-button-2").html(data.button2[0]);
+    $("#decision-text").html(data.text[0]);
+    $(this).css("visibility", "hidden");
+    locationCounter ++;
+  }
+
+  // function for the decision you make
+  function choice() {
+    if (locationCounter === 1) {
+      $(".choice-button-1").html(data.button1[1]);
+      $(".choice-button-2").html(data.button2[1]);
+      $("#decision-text").html(data.text[1]);
+      locationCounter ++;
+    } else if (locationCounter === 1) {
+        $(".choice-button-1").html(data.button1[2]);
+        $(".choice-button-2").html(data.button2[2]);
+        $("#decision-text").html(data.text[2]);
+        locationCounter ++;
+    } else if (locationCounter === 2) {
+        combatReady();
+    }
+  }
+
+  function combatReady() {
+    $(".combat-roll-button").css("visibility", "visible");
+    $(".combat-text").css("visibility", "visible");
+    $(".choice-button-1").css("visibility", "hidden");
+    $(".choice-button-2").css("visibility", "hidden");
+    $("#decision-text").css("visibility", "hidden");
+
+  }
 
   function combat() {
-    $(this).css("visibility", "visible");
-    $(".combat-text").css("visibility", "visible");
     var playerRoll = Math.floor(Math.random() * 6);
     var enemyRoll = Math.floor(Math.random() * 6);
-    console.log(playerRoll);
     if (playerRoll > enemyRoll) {
       var damage = playerRoll - enemyRoll;
       enemyHealth = enemyHealth - damage;
@@ -69,14 +79,16 @@ $(function(){
       console.log("you are dead");
     } else if (enemyHealth <= 0) {
       console.log("the enemy is dead");
+      combatCounter++;
+      choice();
     }
   }
 
-$(".combat-roll-button").click(combat);
 
 
 
-  // setGameListeners();
+
+  setGameListeners();
 
 
   //
