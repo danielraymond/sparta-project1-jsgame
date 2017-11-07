f8b28c85c4d717ba758bff74e5ce334e53b5b975
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-07 10:01:00 +0000

    edited choice options and used location counter to check game progress

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $(function(){
 
+  var location = 0;
+
   var data =  {
     "text":["You are at the entrance to the ... 100 metres into the corridor/tunnel you come across two doors. Do you choose the left door or the right door?", "option2", "option3"],
-    "button1":["go left", "next button 1 text", "another button one text"],
-    "button2":["go right", "next button 2 text", "another button two text"]
+    "button1":["go left", "2a1", "2a2"],
+    "button2":["go right", "2b1", "2b2"]
   }
 
   function setGameListeners() {
@@ -16,18 +18,31 @@ $(function(){
     $("p").css("visibility", "visible");
     $(".choice-button-1").css("visibility", "visible");
     $(".choice-button-2").css("visibility", "visible");
+    $(".choice-button-1").html(data.button1[0]);
+    $(".choice-button-2").html(data.button2[0]);
+    $("p").html(data.text[0]);
     $(this).css("visibility", "hidden");
+    location ++;
   }
 
   function choice1() {
-    var choice = data.button1[1];
-    $(".choice-button-1").html(choice);
+    if (location === 1) {
+      $(".choice-button-1").html(data.button1[1]);
+      $(".choice-button-2").html(data.button2[1]);
+      $("p").html(data.text[1]);
+    }
+    location ++;
   }
 
   function choice2() {
-    var choice = data.button2[1];
-    $(".choice-button-2").html(choice);
+    if (location === 1) {
+      $(".choice-button-1").html(data.button1[2]);
+      $(".choice-button-2").html(data.button2[2]);
+      $("p").html(data.text[2]);
+    }
+    location ++;
   }
+
   setGameListeners();
 
 
