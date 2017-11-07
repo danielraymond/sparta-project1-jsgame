95c4d40610e0b007cf5569e298ba0061f34e0cb0
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-07 16:45:59 +0000

    changed method of changing between options and added text and decisions and added functions remove things

--- script.js
+++ script.js
@@ -3,61 +3,94 @@ $(function(){
   var locationCounter = 0;
   var combatCounter = 0;
   var playerHealth = 12;
+  var winTest = null;
 
   var data =  {
-    "text":["You are at the entrance to the ... 100 metres into the corridor/tunnel you come across two doors. Do you choose the left door or the right door?", "option2", "option3"],
-    "button1":["go left", "2a1", "2a2"],
-    "button2":["go right", "2b1", "2b2"],
-    "enemy":["enemy1", "boss"],
+    "text":["You are at the entrance to the ... 100 metres into the corridor/tunnel you come across two doors. Do you choose the left door or the right door?",
+    "You are in a circular room with a basin of water in the middle. The water looks clear and you feel the urge to drink. There is a door at the far side of the room. Do you ",
+    "You cup your hands and take a drink. You feel a rush of energy and notice no ill effects.",
+    "You have killed the orc. There is nothing else in the room apart from a thick wooden door.",
+    "You find yourself in a room with a huge troll. The troll is guarding the treasure. You must fight the troll to get to the treasure."],
+    "button1":["go left", "Take a drink.", "Walk through the door", "Attack the troll!"],
+    "button2":["go right", "Attack the Orc!", "Ignore the basin and walk through the door.", "Leave through the door."],
+    "enemy":["Goblin", "Troll"],
     "enemyHealth":["6", "10"]
   }
 
-  var enemyHealth = parseInt(data.enemyHealth[combatCounter]);
+  var enemyHealth;
 
   // setting listeners on game buttons and functions that it applies
   function setGameListeners() {
     $("#start-button").click(startGame);
-    $(".choice-button-1").click(choice);
-    $(".choice-button-2").click(choice);
+    $(".choice-button-1").click(choice1);
+    $(".choice-button-2").click(choice2);
     $(".combat-roll-button").click(combat);
   }
 
   // function that happens when the start game button is clicked
   function startGame() {
-    $("#decision-text").css("visibility", "visible");
-    $(".choice-button-1").css("visibility", "visible");
-    $(".choice-button-2").css("visibility", "visible");
+    $("#decision-text").css("display", "inline-block");
+    $(".choice-button-1").css("display", "inline-block");
+    $(".choice-button-2").css("display", "inline-block");
     $(".choice-button-1").html(data.button1[0]);
     $(".choice-button-2").html(data.button2[0]);
     $("#decision-text").html(data.text[0]);
-    $(this).css("visibility", "hidden");
+    $(this).css("display", "none");
     locationCounter ++;
   }
 
   // function for the decision you make
-  function choice() {
-    if (locationCounter === 1) {
-      $(".choice-button-1").html(data.button1[1]);
-      $(".choice-button-2").html(data.button2[1]);
-      $("#decision-text").html(data.text[1]);
-      locationCounter ++;
-    } else if (locationCounter === 1) {
-        $(".choice-button-1").html(data.button1[2]);
-        $(".choice-button-2").html(data.button2[2]);
-        $("#decision-text").html(data.text[2]);
-        locationCounter ++;
-    } else if (locationCounter === 2) {
-        combatReady();
-    }
+  function choice1() {
+    $(".choice-button-1").css("display", "none");
+    $(".choice-button-2").css("display", "none");
+    removeDecisionButtons();
+    winTest = "test";
+    combatCounter = 0;
+    combatReady();
+  }
+
+  function orcWin() {
+    removeCombat();
+    $("#decision-text").css("display", "inline-block");
+    $(".col-md-10").append('<button class="decision-button" id="orcFightWin"></button>')
+    $("#decision-text").html(data.text[3]);
+    $("#orcFightWin").html(data.button2[3]);
+    $("#orcFightWin").click(boss);
+  }
+
+  function choice2() {
+    $(".choice-button-1").css("display", "none");
+    $(".choice-button-2").css("display", "none");
+    $(".col-md-10").append('<button class="decision-button" id="drink"></button>')
+    $(".col-md-10").append('<button class="decision-button" id="noDrink"></button>');
+    $("#decision-text").html(data.text[1]);
+    $("#drink").html(data.button1[1])
+    $("#noDrink").html(data.button2[2])
+    $("#drink").click(drink);
+    $("#noDrink").click(boss);
+  }
+
+  function drink() {
+    removeDecisionButtons();
+    $("#decision-text").html(data.text[2]);
+    playerHealth = playerHealth + 6;
+    $(".col-md-10").append('<button class="decision-button" id="leave"></button>')
+    $("#leave").html(data.button2[3]);
+    $("#leave").click(boss);
+  }
+
+  function boss(){
+    removeDecisionButtons();
+    combatCounter = 1;
+    winTest = "boss";
+    combatReady();
   }
 
   function combatReady() {
-    $(".combat-roll-button").css("visibility", "visible");
-    $(".combat-text").css("visibility", "visible");
-    $(".choice-button-1").css("visibility", "hidden");
-    $(".choice-button-2").css("visibility", "hidden");
-    $("#decision-text").css("visibility", "hidden");
-
+    $(".combat-roll-button").css("display", "inline-block");
+    $(".combat-text").css("display", "inline-block");
+    $("#decision-text").css("display", "none");
+    enemyHealth = parseInt(data.enemyHealth[combatCounter]);
   }
 
   function combat() {
@@ -66,49 +99,58 @@ $(function(){
     if (playerRoll > enemyRoll) {
       var damage = playerRoll - enemyRoll;
       enemyHealth = enemyHealth - damage;
-      console.log("You did " + damage + " damage. The enemy's health is now: " + enemyHealth);
+      $(".combat-text").html("You did " + damage + " damage. The " + data.enemy[combatCounter] + "'s health is now: " + enemyHealth);
     } else if (enemyRoll > playerRoll) {
       var damage = enemyRoll - playerRoll;
       playerHealth = playerHealth - damage;
-      console.log("The enemy did " + damage + " damage. Your health is now: " + playerHealth);
+      $(".combat-text").html("The " + data.enemy[combatCounter] + " did " + damage + " damage. Your health is now: " + playerHealth);
     } else {
-      console.log("You both missed!");
+      $(".combat-text").html("You both missed!");
     }
 
     if (playerHealth <= 0) {
-      console.log("you are dead");
+      $(".combat-text").html("You are dead!");
     } else if (enemyHealth <= 0) {
-      console.log("the enemy is dead");
-      combatCounter++;
-      choice();
+      $(".combat-text").html("The " + data.enemy[combatCounter] + " is dead");
+      victory();
     }
   }
 
+  function removeDecisionButtons() {
+    $(".decision-button").remove();
+  }
 
+  function removeCombat() {
+    $(".combat-text").html("You are in combat")
+    $(".combat-roll-button").css("display", "none");
+    $(".combat-text").css("display", "none");
+  }
 
-
+  function victory() {
+    if (winTest === "test") {
+      orcWin();
+    } else if (winTest === "boss") {
+      removeDecisionButtons();
+      removeCombat();
+      $(".col-md-10").append('<h1>YOU HAVE WON</h1>');
+    }
+  }
 
   setGameListeners();
 
 
-  //
-  // for(i = 0; i < 3; i++) {
-  //   $("button").click(function (){
-  //     $("p").css("visibility", "visible");
-  //     $(".choiceButton1").css("visibility", "visible");
-  //     $(".choiceButton2").css("visibility", "visible");
-  //     $("start-button").css("visibility", "hidden");
-  //     var location = data.text[i];
-  //     debugger;
-  //     $("p").html() = location;
-  //   });
-  //
-  // }
 
 
 
 
 
+function path2() {
+  // Use jquery to create two new buttons and give them ids
+  // Add a new id to them and target that
+  // Add new event listeners to these new buttons
+  // Have the event listeners call new fucntions e.g. path3 or path4
+  // Rinse and repeat
+}
 
 
 
