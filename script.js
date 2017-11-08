8ddfacb1a230ed29cee2bda7b3d0b413fac0a6bc
Tech-A08 <tech-a08@Tech-A08s-MacBook-Air.local>
2017-11-08 12:32:36 +0000

    added new game story and branching

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ $(function(){
   // game options
   var data =  {
     "text":["You are at the entrance to the ... 100 metres into the corridor/tunnel you come across two doors. Do you choose the left door or the right door?",
-    "You are in a circular room with a basin of water in the middle. The water looks clear and you feel the urge to drink. There is a door at the far side of the room. Do you ",
+    "You see a small goblin sleeping in front of you. As you try to slip past your foot knocks against loose metal chain on the floor. The goblin wakes instantly and charges wielding a small dagger!",
+    "You are in a circular room with a basin of water in the middle. The water looks clear and you feel the urge to drink. There is a door at the far side of the room. Do you ...",
     "You cup your hands and take a drink. You feel a rush of energy and notice no ill effects.",
-    "You have killed the orc. There is nothing else in the room apart from a large wooden door.",
+    "You have killed the goblin. There are three doors to choose from. There is a bright light coming from behind the left door. The middle door looks heavier and has iron studs dotted over it. The left door looks to be the most used of the three as the door knob is worn smooth with use. Do you ...",
     "You find yourself in a room with a huge troll. The troll is guarding the treasure. You must fight the troll to get to the treasure."],
-    "button1":["go left", "Take a drink.", "Walk through the door", "Attack the troll!"],
-    "button2":["go right", "Attack the Orc!", "Ignore the basin and walk through the door.", "Leave through the door."],
-    "enemy":["Goblin", "Troll"],
-    "enemyHealth":["6", "10"]
+    "button":["go left", "go right", "go through the left door", "go through the middle door", "go through the right door", "Take a drink.", "Walk through the door", "Attack the troll!", "Attack the Goblin!", "Ignore the basin and walk through the door.", "Leave through the door."],
+    "enemy":["Goblin", "Troll", "Giant Spider", "enemy4", "enemy5", "enemy6"],
+    "enemyHealth":["4", "10"]
   }
 
   var enemyHealth;
@@ -35,8 +35,8 @@ $(function(){
     $("#decision-text").css("display", "inline-block");
     $(".choice-button-1").css("display", "inline-block");
     $(".choice-button-2").css("display", "inline-block");
-    $(".choice-button-1").html(data.button1[0]);
-    $(".choice-button-2").html(data.button2[0]);
+    $(".choice-button-1").html(data.button[0]);
+    $(".choice-button-2").html(data.button[1]);
     $("#decision-text").html(data.text[0]);
     $(this).css("display", "none");
     locationCounter ++;
@@ -47,18 +47,27 @@ $(function(){
     $(".choice-button-1").css("display", "none");
     $(".choice-button-2").css("display", "none");
     removeDecisionButtons();
+    $(".col-md-10").append('<button class="decision-button-1" id="attackGoblin"></button>');
+    $("#decision-text").html(data.text[1]);
+    $('#attackGoblin').html(data.button[8]);
     winTest = "test";
     combatCounter = 0;
-    combatReady();
+    $('#attackGoblin').click(combatReady);
   }
 
   function orcWin() {
     removeCombat();
     $("#decision-text").css("display", "inline-block");
-    $(".col-md-10").append('<button class="decision-button-1" id="orcFightWin"></button>')
-    $("#decision-text").html(data.text[3]);
-    $("#orcFightWin").html(data.button2[3]);
-    $("#orcFightWin").click(boss);
+    $(".col-md-10").append('<button class="decision-button-3" id="orcFightLeft"></button>')
+    $(".col-md-10").append('<button class="decision-button-3" id="orcFightMiddle"></button>')
+    $(".col-md-10").append('<button class="decision-button-3" id="orcFightRight"></button>')
+    $("#decision-text").html(data.text[4]);
+    $("#orcFightLeft").html(data.button[2])
+    $("#orcFightMiddle").html(data.button[3])
+    $("#orcFightRight").html(data.button[4])
+    $("#orcFightLeft").click(orcGoLeft);
+    $("#orcFightMiddle").click(orcGoMiddle);
+    $("#orcFightRight").click(orcGoRight);
   }
 
   function choice2() {
@@ -66,21 +75,35 @@ $(function(){
     $(".choice-button-2").css("display", "none");
     $(".col-md-10").append('<button class="decision-button-2" id="drink"></button>')
     $(".col-md-10").append('<button class="decision-button-2" id="noDrink"></button>');
-    $("#decision-text").html(data.text[1]);
-    $("#drink").html(data.button1[1])
-    $("#noDrink").html(data.button2[2])
+    $("#decision-text").html(data.text[2]);
+    $("#drink").html(data.button[5])
+    $("#noDrink").html(data.button[9])
     $("#drink").click(drink);
     $("#noDrink").click(boss);
   }
 
   function drink() {
     removeDecisionButtons();
-    $("#decision-text").html(data.text[2]);
+    $("#decision-text").html(data.text[3]);
     playerHealth = playerHealth + 6;
     $(".col-md-10").append('<button class="decision-button-1" id="leave"></button>')
-    $("#leave").html(data.button2[3]);
+    $("#leave").html(data.button[6]);
     $("#leave").click(boss);
   }
+
+  function orcGoLeft() {
+    removeDecisionButtons();
+
+  }
+
+  function orcGoMiddle() {
+    removeDecisionButtons();
+  }
+
+  function orcGoRight() {
+    removeDecisionButtons();
+  }
+
 
   function boss(){
     removeDecisionButtons();
@@ -91,6 +114,7 @@ $(function(){
 
   // function to prepare screen for combat
   function combatReady() {
+    removeDecisionButtons();
     $(".combat-roll-button").css("display", "inline-block");
     $(".combat-text").css("display", "inline-block");
     $("#decision-text").css("display", "none");
@@ -127,6 +151,7 @@ $(function(){
   function removeDecisionButtons() {
     $(".decision-button-1").remove();
     $(".decision-button-2").remove();
+    $(".decision-button-3").remove();
   }
 
   // function to remove combat screen once combat is completed
@@ -171,5 +196,5 @@ $(function(){
   }
 
   setGameListeners();
-  
+
 })
